3698c020627ad83f6c03e95cd3b581993302375f
MatiaszG <101513347+MatiaszG@users.noreply.github.com>
2022-05-18 21:41:02 +0200

    Dodanie funkcji rozdającej karty.

--- blackjack/blackjack.cpp
+++ blackjack/blackjack.cpp
@@ -20,10 +20,25 @@ void shuffle_deck(vector<int> &deck)
     random_shuffle(deck.begin(), deck.end());
 }
 
+void deal(vector<int> &deck, vector<int> &user_cards, vector<int> &dealer_cards)
+{
+    for (int i = 0; i < 2; i++)
+    {
+        dealer_cards.push_back(deck.back());
+        deck.pop_back();
+        user_cards.push_back(deck.back());
+        deck.pop_back();
+    }
+}
+
 int main()
 {
     vector<int> deck;
+    vector<int> user_cards;
+    vector<int> dealer_cards;
     initialize_deck(deck);
     shuffle_deck(deck);
+    deal(deck, user_cards, dealer_cards);
+
     return 0;
 }
